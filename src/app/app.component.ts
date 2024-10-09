a0cd67e6434e936ab8589328925e3898fce81323
etwicaksono <ekoteguhwicaksono@gmail.com>
2024-10-09 14:01:40 +0700

    Change version to no standalone config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {LoginComponent} from "./pages/login/login.component";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
