2d2aba742eb7ab0425e211f621579a55f6dbb795
etwicaksono <ekoteguhwicaksono@gmail.com>
2024-10-14 09:25:54 +0700

    Implement refresh token feature

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,6 +3,8 @@ import {BehaviorSubject, Observable, tap} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environment/environment";
 import {UserResponseDto} from "../dto/user-response.dto";
+import {toZonedTime, fromZonedTime} from 'date-fns-tz';
+import {NotificationService} from "./notification.service";
 
 @Injectable({
   providedIn: 'root'
@@ -11,17 +13,20 @@ export class AuthService {
   // BehaviorSubject to hold the current state of login
   private readonly loggedIn = new BehaviorSubject<boolean>(false);
   private readonly userRole = new BehaviorSubject<string>('');
-  private readonly userName = new BehaviorSubject<string>('');
+  private readonly tokenKey = 'token'
+  private readonly userKey = 'user'
+  private readonly tokenExpiredKey = 'tokenExpired'
+  private readonly tokenRefreshableKey = 'tokenRefreshable'
 
   // Observable for other components to subscribe to
   public isLoggedIn$ = this.loggedIn.asObservable();
   public userRole$ = this.userRole.asObservable();
-  public userName$ = this.userName.asObservable();
 
   constructor(
     private readonly http: HttpClient,
+    private readonly notificationService: NotificationService,
   ) {
-    const isLoggedIn = !!localStorage.getItem('token'); // Check if token exists
+    const isLoggedIn = !!localStorage.getItem(this.tokenKey); // Check if token exists
     this.loggedIn.next(isLoggedIn);
   }
 
@@ -32,15 +37,35 @@ export class AuthService {
           if (response.success == 1) {
             this.loggedIn.next(true);
             this.userRole.next(response.data.role);
-            localStorage.setItem('token', `Bearer ${response.data.token}`);  // Save the token in localStorage
+            localStorage.setItem(this.tokenKey, `Bearer ${response.data.token}`)
+            localStorage.setItem(this.tokenExpiredKey, response.meta.expired_at)
+            localStorage.setItem(this.tokenRefreshableKey, response.meta.refreshable_till)
+          }
+        })
+      );
+  }
+
+  refreshToken(): Observable<any> {
+    const bearerToken = localStorage.getItem(this.tokenKey)
+    return this.http.post(`${environment.apiURl}/auth/refresh-token`, null, {headers: {'Authorization': `Bearer ${bearerToken}`}})
+      .pipe(
+        tap((response: any) => {
+          if (response.success == 1) {
+            this.loggedIn.next(true);
+            this.userRole.next(response.data.role);
+            localStorage.setItem(this.tokenKey, `Bearer ${response.data.token}`)
+            localStorage.setItem(this.tokenExpiredKey, response.meta.expired_at)
+            localStorage.setItem(this.tokenRefreshableKey, response.meta.refreshable_till)
           }
         })
       );
   }
 
   logout(): Observable<any> {
-    const bearerToken = localStorage.getItem('token')
-    localStorage.removeItem('token');
+    const bearerToken = localStorage.getItem(this.tokenKey)
+    localStorage.removeItem(this.tokenKey);
+    localStorage.removeItem(this.tokenExpiredKey);
+    localStorage.removeItem(this.tokenRefreshableKey);
     this.loggedIn.next(false);
     this.userRole.next('');
     return this.http.post(`${environment.apiURl}/auth/logout`, null, {headers: {'Authorization': `Bearer ${bearerToken}`}});
@@ -48,12 +73,52 @@ export class AuthService {
 
   // Method to check if the user is logged in
   isLoggedIn(): boolean {
-    return !!localStorage.getItem('token');
+    // Get the current time in UTC
+    const currentTime = new Date();
+    const currentTimeUTC = toZonedTime(currentTime, 'UTC');
+    // Get expired time in UTC
+    const storedExpiredAuth = localStorage.getItem(this.tokenExpiredKey)
+    const storedExpiredAuthUTC = toZonedTime(new Date(`${storedExpiredAuth}Z`), 'UTC'); // Append 'Z' to indicate it's UTC
+    // Get refreshable time in UTC
+    const storedRefreshable = localStorage.getItem(this.tokenRefreshableKey)
+    const storedRefreshableUTC = toZonedTime(new Date(`${storedRefreshable}Z`), 'UTC'); // Append 'Z' to indicate it's UTC
+
+    // When token is expired
+    if (currentTimeUTC > storedExpiredAuthUTC) {
+      // When token is refreshable
+      if (storedRefreshableUTC > currentTimeUTC) {
+        // Refresh token
+        console.log('Token is expired, refresh token');
+        this.refreshToken().subscribe({
+          next: () => {
+            this.notificationService.showSuccess('Token refreshed');
+          },
+          error: (err) => {
+            console.error('Refresh token failed', err);
+            this.notificationService.showError(err.error.message);
+          }
+        })
+      } else {
+        // Do logout
+        console.log('Token is expired, do logout');
+        this.logout().subscribe({
+          next: () => {
+            this.notificationService.showSuccess('Logout successful');
+          },
+          error: (err) => {
+            console.error('Logout failed', err);
+            this.notificationService.showError(err.error.message);
+          }
+        })
+      }
+    }
+
+    return !!localStorage.getItem(this.tokenKey);
   }
 
   // Method to get the user profile data
   async getUserProfile(): Promise<UserResponseDto> {
-    const user = localStorage.getItem('user')
+    const user = localStorage.getItem(this.userKey)
     let userData = new UserResponseDto()
 
     if (user) {
@@ -61,19 +126,18 @@ export class AuthService {
       userData = JSON.parse(user);
     } else {
       console.debug('retrieve data from api')
-      const bearerToken = localStorage.getItem('token')
+      const bearerToken = localStorage.getItem(this.tokenKey)
       await this.http.get(`${environment.apiURl}/my-profile`, {headers: {'Authorization': `Bearer ${bearerToken}`}})
         .pipe(
           tap((response: any) => {
             if (response.success == 1) {
-              localStorage.setItem('user', JSON.stringify(response.data));  // Save the token in localStorage
+              localStorage.setItem(this.userKey, JSON.stringify(response.data));  // Save the token in localStorage
               userData = response.data
             }
           })
         ).toPromise();
     }
 
-    this.userName.next(userData.name);
     return userData
   }
 
